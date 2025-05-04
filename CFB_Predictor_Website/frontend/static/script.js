6892ef5f61f7b72852f2b4711364d63d218ac648
Grady Johnson <gradyjohnson@Gradys-MacBook-Pro.local>
2025-05-04 11:00:31 -0700

    Adding list for team search

--- CFB_Predictor_Website/frontend/static/script.js
+++ CFB_Predictor_Website/frontend/static/script.js
@@ -1,8 +1,24 @@
+import { teamMapping, teamNames } from 'teammapping.js';
+
+
+const datalist = document.getElementById('team-options');
+teamNames.forEach(team => {
+  const option = document.createElement('option');
+  option.value = team;
+  datalist.appendChild(option);
+});
+
+function normalizeTeam(name) {
+  return teamMapping[name] || name;
+}
+
 document.getElementById("predict-form").addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  const team1 = document.getElementById("team1").value;
-  const team2 = document.getElementById("team2").value;
+  const rawTeam1 = document.getElementById("team1").value.trim();
+  const rawTeam2 = document.getElementById("team2").value.trim();
+  const team1 = normalizeTeam(rawTeam1);
+  const team2 = normalizeTeam(rawTeam2);
   const week = document.getElementById("week").value;
 
   const response = await fetch("https://predictorfile.onrender.com/predict", {
---
+++ CFB_Predictor_Website/frontend/static/teammapping.js
@@ -0,0 +1,348 @@
+// Canonical team mapping with common variants
+export const teamMapping = {
+    'Florida Intl': 'FIU',
+    'FIU': 'FIU',
+    'Florida International': 'FIU',
+  
+    'Ohio St': 'Ohio State',
+    'Ohio State': 'Ohio State',
+    'OSU': 'Ohio State',
+  
+    'Iowa St': 'Iowa State',
+    'Iowa State': 'Iowa State',
+  
+    'Notre Dame': 'Notre Dame',
+  
+    'N Illinois': 'Northern Illinois',
+    'Northern Illinois': 'Northern Illinois',
+  
+    'Washington': 'Washington',
+  
+    'Texas': 'Texas',
+  
+    'LA Tech': 'Louisiana Tech',
+    'Louisiana Tech': 'Louisiana Tech',
+  
+    'Sam Hous St': 'Sam Houston State',
+    'Sam Houston State': 'Sam Houston State',
+  
+    'UL Monroe': 'Louisiana-Monroe',
+    'Louisiana-Monroe': 'Louisiana-Monroe',
+  
+    'Minnesota': 'Minnesota',
+  
+    'Indiana': 'Indiana',
+  
+    'Bowling Grn': 'Bowling Green',
+    'Bowling Green': 'Bowling Green',
+  
+    'Wisconsin': 'Wisconsin',
+  
+    'W Kentucky': 'Western Kentucky',
+    'Western Kentucky': 'Western Kentucky',
+  
+    'Tulane': 'Tulane',
+  
+    'Kentucky': 'Kentucky',
+  
+    'Rice': 'Rice',
+  
+    'Alabama': 'Alabama',
+  
+    'Oregon': 'Oregon',
+  
+    'BYU': 'Brigham Young',
+    'Brigham Young': 'Brigham Young',
+  
+    'Houston': 'Houston',
+  
+    'U Mass': 'Massachusetts',
+    'UMass': 'Massachusetts',
+    'Massachusetts': 'Massachusetts',
+  
+    'UAB': 'Alabama-Birmingham',
+    'Alabama-Birmingham': 'Alabama-Birmingham',
+  
+    'Army': 'Army',
+  
+    'Temple': 'Temple',
+  
+    'Tennessee': 'Tennessee',
+  
+    'Penn St': 'Penn State',
+    'Penn State': 'Penn State',
+  
+    'Colorado': 'Colorado',
+  
+    'Miami (OH)': 'Miami (OH)',
+  
+    'Missouri': 'Missouri',
+  
+    'TX Christian': 'Texas Christian',
+    'TCU': 'Texas Christian',
+    'Texas Christian': 'Texas Christian',
+  
+    'Louisiana': 'Louisiana',
+  
+    'Liberty': 'Liberty',
+  
+    'Air Force': 'Air Force',
+  
+    'Florida St': 'Florida State',
+    'Florida State': 'Florida State',
+  
+    'Nevada': 'Nevada',
+  
+    'Marshall': 'Marshall',
+  
+    'S Carolina': 'South Carolina',
+    'South Carolina': 'South Carolina',
+  
+    'Central Mich': 'Central Michigan',
+    'Central Michigan': 'Central Michigan',
+  
+    'Fresno St': 'Fresno State',
+    'Fresno State': 'Fresno State',
+  
+    'Georgia St': 'Georgia State',
+    'Georgia State': 'Georgia State',
+  
+    'Oklahoma': 'Oklahoma',
+  
+    'Iowa': 'Iowa',
+  
+    'James Mad': 'James Madison',
+    'James Madison': 'James Madison',
+  
+    'Toledo': 'Toledo',
+  
+    'Utah': 'Utah',
+  
+    'Charlotte': 'Charlotte',
+  
+    'Oregon St': 'Oregon State',
+    'Oregon State': 'Oregon State',
+  
+    'Texas St': 'Texas State',
+    'Texas State': 'Texas State',
+  
+    'Navy': 'Navy',
+  
+    'Auburn': 'Auburn',
+  
+    'Georgia': 'Georgia',
+  
+    'Michigan St': 'Michigan State',
+    'Michigan State': 'Michigan State',
+  
+    'Troy': 'Troy',
+  
+    'VA Tech': 'Virginia Tech',
+    'Virginia Tech': 'Virginia Tech',
+  
+    'Jksnville St': 'Jacksonville State',
+    'Jacksonville State': 'Jacksonville State',
+  
+    'Michigan': 'Michigan',
+  
+    'Ohio': 'Ohio',
+  
+    'Connecticut': 'Connecticut',
+    'UConn': 'Connecticut',
+  
+    'S Mississippi': 'Southern Mississippi',
+    'Southern Miss': 'Southern Mississippi',
+    'Southern Mississippi': 'Southern Mississippi',
+  
+    'Miami': 'Miami',
+  
+    'Cincinnati': 'Cincinnati',
+  
+    'Nebraska': 'Nebraska',
+  
+    'Duke': 'Duke',
+  
+    'Clemson': 'Clemson',
+  
+    'San Diego St': 'San Diego State',
+    'San Diego State': 'San Diego State',
+  
+    'App State': 'Appalachian State',
+    'Appalachian State': 'Appalachian State',
+  
+    'Hawaii': 'Hawaii',
+  
+    'Arizona St': 'Arizona State',
+    'Arizona State': 'Arizona State',
+  
+    'California': 'California',
+  
+    'Wyoming': 'Wyoming',
+  
+    'UCF': 'Central Florida',
+    'Central Florida': 'Central Florida',
+  
+    'Syracuse': 'Syracuse',
+  
+    'LSU': 'LSU',
+  
+    'E Michigan': 'Eastern Michigan',
+    'Eastern Michigan': 'Eastern Michigan',
+  
+    'Illinois': 'Illinois',
+  
+    'GA Tech': 'Georgia Tech',
+    'Georgia Tech': 'Georgia Tech',
+  
+    'W Michigan': 'Western Michigan',
+    'Western Michigan': 'Western Michigan',
+  
+    'Coastal Car': 'Coastal Carolina',
+    'Coastal Carolina': 'Coastal Carolina',
+  
+    'TX El Paso': 'Texas-El Paso (UTEP)',
+    'UTEP': 'Texas-El Paso (UTEP)',
+    'Texas-El Paso': 'Texas-El Paso (UTEP)',
+    'Texas-El Paso (UTEP)': 'Texas-El Paso (UTEP)',
+  
+    'NC State': 'North Carolina State',
+    'North Carolina State': 'North Carolina State',
+  
+    'S Methodist': 'Southern Methodist',
+    'SMU': 'Southern Methodist',
+    'Southern Methodist': 'Southern Methodist',
+  
+    'Florida': 'Florida',
+  
+    'Kansas St': 'Kansas State',
+    'Kansas State': 'Kansas State',
+  
+    'USC': 'Southern California',
+    'Southern California': 'Southern California',
+  
+    'UNLV': 'Nevada-Las Vegas',
+    'Nevada-Las Vegas': 'Nevada-Las Vegas',
+  
+    'San Jose St': 'San Jose State',
+    'San Jose State': 'San Jose State',
+  
+    'Northwestern': 'Northwestern',
+  
+    'N Carolina': 'North Carolina',
+    'North Carolina': 'North Carolina',
+  
+    'Mississippi': 'Mississippi',
+  
+    'Old Dominion': 'Old Dominion',
+  
+    'Colorado St': 'Colorado State',
+    'Colorado State': 'Colorado State',
+  
+    'Akron': 'Akron',
+  
+    'Rutgers': 'Rutgers',
+  
+    'Texas A&M': 'Texas A&M',
+  
+    'Fla Atlantic': 'Florida Atlantic',
+    'Florida Atlantic': 'Florida Atlantic',
+  
+    'UCLA': 'UCLA',
+  
+    'Miss State': 'Mississippi State',
+    'Mississippi State': 'Mississippi State',
+  
+    'Baylor': 'Baylor',
+  
+    'Kennesaw St': 'Kennesaw State',
+    'Kennesaw State': 'Kennesaw State',
+  
+    'N Mex State': 'New Mexico State',
+    'New Mexico St': 'New Mexico State',
+    'New Mexico State': 'New Mexico State',
+  
+    'E Carolina': 'East Carolina',
+    'East Carolina': 'East Carolina',
+  
+    'Maryland': 'Maryland',
+  
+    'Kansas': 'Kansas',
+  
+    'Louisville': 'Louisville',
+  
+    'Boise St': 'Boise State',
+    'Boise State': 'Boise State',
+  
+    'Middle Tenn': 'Middle Tennessee State',
+    'Middle Tennessee': 'Middle Tennessee State',
+    'Middle Tennessee State': 'Middle Tennessee State',
+  
+    'Buffalo': 'Buffalo',
+  
+    'Arizona': 'Arizona',
+  
+    'Vanderbilt': 'Vanderbilt',
+  
+    'Arkansas St': 'Arkansas State',
+    'Arkansas State': 'Arkansas State',
+  
+    'S Alabama': 'South Alabama',
+    'South Alabama': 'South Alabama',
+  
+    'Kent St': 'Kent State',
+    'Kent State': 'Kent State',
+  
+    'Arkansas': 'Arkansas',
+  
+    'Utah St': 'Utah State',
+    'Utah State': 'Utah State',
+  
+    'Boston Col': 'Boston College',
+    'Boston College': 'Boston College',
+  
+    'GA Southern': 'Georgia Southern',
+    'Georgia Southern': 'Georgia Southern',
+  
+    'Pittsburgh': 'Pittsburgh',
+  
+    'W Virginia': 'West Virginia',
+    'West Virginia': 'West Virginia',
+  
+    'Memphis': 'Memphis',
+  
+    'Purdue': 'Purdue',
+  
+    'North Texas': 'North Texas',
+  
+    'Wash State': 'Washington State',
+    'Washington State': 'Washington State',
+  
+    'Virginia': 'Virginia',
+  
+    'UTSA': 'Texas-San Antonio (UTSA)',
+    'Texas-San Antonio': 'Texas-San Antonio (UTSA)',
+    'Texas San Antonio': 'Texas-San Antonio (UTSA)',
+    'Texas-San Antonio (UTSA)': 'Texas-San Antonio (UTSA)',
+  
+    'Ball St': 'Ball State',
+    'Ball State': 'Ball State',
+  
+    'New Mexico': 'New Mexico',
+  
+    'Stanford': 'Stanford',
+  
+    'Oklahoma St': 'Oklahoma State',
+    'Oklahoma State': 'Oklahoma State',
+  
+    'Wake Forest': 'Wake Forest',
+  
+    'Texas Tech': 'Texas Tech',
+  
+    'S Florida': 'South Florida',
+    'South Florida': 'South Florida',
+  
+    'Tulsa': 'Tulsa'
+  };
+  
+  export const teamNames = [...new Set(Object.values(team_mapping))].sort();
+  
